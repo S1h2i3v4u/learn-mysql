1d45abfe28b45468d077cb086204b7d1ed348bab
Shivani Suryawansi <158851522+S1h2i3v4u@users.noreply.github.com>
2025-08-21 22:05:03 +0530

    Update DQL.sql

--- sql-scripts/DQL/DQL.sql
+++ sql-scripts/DQL/DQL.sql
@@ -1,1 +1,13 @@
+SELECT * FROM employee;
+
+SELECT emp_id,name FROM employee;
+
+SELECT salary FROM employee
+WHERE department = 'IT';
+
+SELECT name,department from employee
+WHERE salary > 20000;
+
+SELECT emp_id,name,salary FROM employee
+ORDER BY salary DESC , emp_id ASC;
 
