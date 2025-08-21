0c9d77cf09e993760e8c416860d4d9020dceca0a
Shivani Suryawansi <158851522+S1h2i3v4u@users.noreply.github.com>
2025-08-21 21:47:13 +0530

    Update DML.sql

--- sql-scripts/DML/DML.sql
+++ sql-scripts/DML/DML.sql
@@ -1,1 +1,26 @@
+CREATE TABLE department (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(100),
+    location VARCHAR(100),
+    manager_id INT
+);
+
+INSERT INTO department (dept_id,dept_name, location, manager_id)
+VALUES
+(1,'IT', 'Pune', NULL),
+(2,'CS', 'Mumbai', NULL);
+
+SELECT * FROM department;
+
+UPDATE department
+SET dept_name = 'ECE'
+WHERE dept_id = 2;
+
+SELECT * FROM department;
+
+DELETE FROM department
+WHERE dept_id = 2;
+
+SELECT * FROM department;
+
 
